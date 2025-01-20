f554f37c25797eac1f5a0410001d2ab2d5db602f
sabreenahmed21 <sabreenahmed212@gmail.com>
2025-01-20 11:05:36 +0200

    remove framer-motion from new&featured products

--- Client/src/pages/Products/NewFeaturedProducts.js
+++ Client/src/pages/Products/NewFeaturedProducts.js
@@ -2,7 +2,6 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import FeaturedProducts from "./FeaturedProducts";
 import NewProducts from "./NewProducts";
-import { motion } from 'framer-motion';
 
 export default function NewFeaturedProducts() {
   const isLargeScreen = useMediaQuery("(min-width:900px)");
@@ -10,11 +9,6 @@ export default function NewFeaturedProducts() {
   const { i18n } = useTranslation();
   const storedLanguage = i18n.language;
   return (
-    <motion.div
-    initial={{ opacity: 0, y: 150 }}
-    whileInView={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5, delay:0.5 }}
-  >
     <Box
       sx={{
         display: isLargeScreen? "grid" : "block",
@@ -29,6 +23,5 @@ export default function NewFeaturedProducts() {
       <FeaturedProducts />
       <NewProducts />
     </Box>
-  </motion.div>
   );
 }
